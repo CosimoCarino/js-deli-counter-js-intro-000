af5c23248aa76fc1b88d28ef2262af3ef4f761d4
Tess Guenthner <tessmadeleine@hotmail.com>
2019-02-26 19:02:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function takeANumber(line, newPerson) {
   line.push(newPerson)
   return `Welcome, ${newPerson}. You are number ${n} in line.`
   }
+  
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+  else {
+    return `Currently serving ${katzDeliLine[0]}.`
+  }
+katzDeliLine.shift( )
+  }
