e87cbe91e9feed965524cea079a2e46ee49a32f7
Tess Guenthner <tessmadeleine@hotmail.com>
2019-02-26 18:49:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(line, newPerson) {
   var n = line.length + 1
-  return `Welcome, ${newPerson}. You are number ${n} in line."`
+  return `Welcome, ${newPerson}. You are number ${n} in line.`
   }
