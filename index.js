47e87c3e27719f266dd11453984f90146407dcb7
Tess Guenthner <tessmadeleine@hotmail.com>
2019-02-26 19:02:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function takeANumber(line, newPerson) {
   line.push(newPerson)
   return `Welcome, ${newPerson}. You are number ${n} in line.`
   }
-  
+
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
